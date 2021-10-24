0daaa0e2a1216821e65d6a8c186f38fa5a71b6cd
ayaf-bless <blessambel1@gmail.com>
2021-10-24 15:41:21 +0300

    add entinty

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,27 +1,39 @@
 import { Module } from "@nestjs/common";
 import { GraphQLModule } from "@nestjs/graphql";
-import { join } from "path";
 import { RestaurentModule } from "./restaurent/restaurent.module";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { ConfigModule } from "@nestjs/config";
+import * as Joi from "joi";
+import { Restaurent } from "./restaurent/entities/restaurent.entity";
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+      ignoreEnvFile: process.env.NODE_ENV === "prod",
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid("dev", "test", "prod"),
+        DB_PORT: Joi.string().required(),
+        DB_HOST: Joi.string().required(),
+        DB_USER: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+      }),
     }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
     }),
     TypeOrmModule.forRoot({
       type: "postgres",
-      port: 5432,
-      host: "localhost",
-      username: "bless_ayaf",
-      database: "nuber-eats",
-      password: "1122",
-      synchronize: true,
-      logging: true,
+      port: +process.env.DB_PORT,
+      host: process.env.DB_HOST,
+      username: process.env.DB_USER,
+      database: process.env.DB_NAME,
+      password: process.env.DB_PASSWORD,
+      synchronize: process.env.NODE_ENV !== "prod",
+      logging: process.env.NODE_ENV !== "prod",
+      entities: [Restaurent],
     }),
     RestaurentModule,
   ],
--- src/restaurent/entities/restaurent.entity.ts
+++ src/restaurent/entities/restaurent.entity.ts
@@ -1,14 +1,23 @@
 import { Field, ObjectType } from "@nestjs/graphql";
 import { IsString, Min } from "class-validator";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @ObjectType()
+@Entity()
 export class Restaurent {
+  @Column()
+  @Field((_type) => String)
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
   @Field((_type) => String)
   @IsString()
   @Min(5)
+  @Column()
   name: string;
 
   @Field((_type) => String, { nullable: true })
   @IsString()
+  @Column()
   description?: string;
 }

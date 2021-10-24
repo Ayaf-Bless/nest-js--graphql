0f4b1bc2b7cd648410dea6ddf94f874df362daae
ayaf-bless <blessambel1@gmail.com>
2021-10-24 14:36:35 +0300

    add typeOrm

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,26 @@ import { Module } from "@nestjs/common";
 import { GraphQLModule } from "@nestjs/graphql";
 import { join } from "path";
 import { RestaurentModule } from "./restaurent/restaurent.module";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      port: 5432,
+      host: "localhost",
+      username: "bless_ayaf",
+      database: "nuber-eats",
+      password: "1122",
+      synchronize: true,
+      logging: true,
     }),
     RestaurentModule,
   ],
